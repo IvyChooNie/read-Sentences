d18951fcc4ad543aedde747f416e02ad1046f314
IvyChooNie <72536620+IvyChooNie@users.noreply.github.com>
2021-01-09 02:12:46 +0800

    Add files via upload

---
+++ CollisionDetect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionDetect : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collided with" + collision.gameObject.name);
+
+
+    }
+
+}
---
+++ scoreDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreDisplay : MonoBehaviour
+{
+    public static int score = 0;
+
+    Text scr;
+
+    void Start()
+    {
+        scr = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scr.text = "Score: " + score;
+    }
+}
