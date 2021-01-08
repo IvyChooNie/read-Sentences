6774628d02c16a9062be8670701021b9f19bd927
IvyChooNie <72536620+IvyChooNie@users.noreply.github.com>
2021-01-09 01:40:50 +0800

    Add files via upload

---
+++ firstScene.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class firstScene : MonoBehaviour
+{
+    public void load()
+    {
+        switch (this.gameObject.name)
+        {
+            case "Back":
+                SceneManager.LoadScene("a-zScene");
+                break;
+        }
+
+        switch (this.gameObject.name)
+        {
+            case "Back01":
+                SceneManager.LoadScene("firstScene");
+                break;
+
+            case "consonants":
+                SceneManager.LoadScene("consonant");
+                break;
+
+            case "vowels":
+                SceneManager.LoadScene("vowel");
+                break;
+
+            case "ResumeToAbc":
+                SceneManager.LoadScene("a-zScene");
+                break;
+
+            case "goToQue":
+                SceneManager.LoadScene("MainMenu");
+                break;
+
+            case "backToFirstScene":
+                SceneManager.LoadScene("firstScene");
+                break;
+        }
+    }
+}
