41942f2d5ec795a1aea41c7c07d1f4730d14bf78
IvyChooNie <72536620+IvyChooNie@users.noreply.github.com>
2021-01-09 01:06:22 +0800

    Add files via upload

---
+++ button/azButton.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class azButton : MonoBehaviour
+{
+
+    public void load()
+    {
+        switch (this.gameObject.name)
+        {
+            case "a":
+                SceneManager.LoadScene("a");
+                break;
+
+
+
+            case "b":
+                SceneManager.LoadScene("b");
+                break;
+
+
+
+            case "c":
+                SceneManager.LoadScene("c");
+                break;
+
+            case "d":
+                SceneManager.LoadScene("d");
+                break;
+
+            case "e":
+                SceneManager.LoadScene("e");
+                break;
+
+            case "f":
+                SceneManager.LoadScene("f");
+                break;
+
+            case "g":
+                SceneManager.LoadScene("g");
+                break;
+
+            case "h":
+                SceneManager.LoadScene("h");
+                break;
+
+            case "i":
+                SceneManager.LoadScene("i");
+                break;
+
+            case "j":
+                SceneManager.LoadScene("j");
+                break;
+
+            case "k":
+                SceneManager.LoadScene("k");
+                break;
+
+            case "l":
+                SceneManager.LoadScene("l");
+                break;
+
+            case "m":
+                SceneManager.LoadScene("m");
+                break;
+
+            case "n":
+                SceneManager.LoadScene("n");
+                break;
+
+            case "o":
+                SceneManager.LoadScene("o");
+                break;
+
+            case "p":
+                SceneManager.LoadScene("p");
+                break;
+
+            case "q":
+                SceneManager.LoadScene("q");
+                break;
+
+            case "r":
+                SceneManager.LoadScene("r");
+                break;
+
+            case "s":
+                SceneManager.LoadScene("s");
+                break;
+
+            case "t":
+                SceneManager.LoadScene("t");
+                break;
+
+            case "u":
+                SceneManager.LoadScene("u");
+                break;
+
+            case "v":
+                SceneManager.LoadScene("v");
+                break;
+
+            case "w":
+                SceneManager.LoadScene("w");
+                break;
+
+            case "x":
+                SceneManager.LoadScene("x");
+                break;
+
+            case "y":
+                SceneManager.LoadScene("y");
+                break;
+
+            case "z":
+                SceneManager.LoadScene("z");
+                break;
+        }
+
+
+    }
+
+}
