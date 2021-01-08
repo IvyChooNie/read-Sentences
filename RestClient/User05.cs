068278cb8d973359ef1e8c9693b02b62259c1e1d
IvyChooNie <72536620+IvyChooNie@users.noreply.github.com>
2021-01-09 01:00:02 +0800

    Add files via upload

---
+++ RestClient/User05.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class User05
+{
+    public string userName;
+    public int userScore;
+
+    public User05()
+    {
+        userScore = q10.playerScore;
+        userName = q10.playerName;
+    }
+}
---
+++ RestClient/User07.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class User07
+{
+    public string userName;
+    public int userScore;
+
+    public User07()
+    {
+        userScore = q06.playerScore;
+        userName = q06.playerName;
+    }
+}
+
